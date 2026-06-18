f684840a606d8e12faf4711d283c4ff55dd0070e
Pixeloko <lola-louna@outlook.fr>
2026-06-18 18:18:15 +0200

    intelligence script exercise

--- Bash_Courses/Exercises/13_os-intel.sh
+++ Bash_Courses/Exercises/13_os-intel.sh
@@ -1,43 +1,56 @@
-#/bin/bash
+#!/usr/local/bin/bash
 
-# intel commands
-commands=(
-    "OS_name=$(uname -o)"
-    "processor=$(uname -p)"
-    "kernel_version_date=$(uname -v | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}')"
-    "architecture=$(arch)"
-    "user_name=$(whoami)"
-    "os_ip=$(hostname -I | tr ' ' '/n')"
-    "user_id=$(id | awk -F'uid=' '{print $2}' | grep -oE -m1 '^[0-9]{1,4}')"
-    "group_id=$(id | awk -F'gid=' '{print $2}' | grep -oE -m1 '^[0-9]{1,4}')"
-    "is_passwd_accessible=$(./passwd_access.sh)"
-)
-
-# handling errors
-for cmd in "${commands[@]}"; do
-    if ! output=$(${cmd} 2>&1); then
-        echo "Error on ${cmd} : ${output}"
-    fi
-done
 if [[ $(uname) != "Linux" ]]; then
     echo "intel gathering impossible or your machine is not Linux."
-    exit 1
 fi
 
+# intel commands, use declare to keep keys->values
+declare -A commands=(
+    [OS_name]="uname -o"
+    [processor]="uname -p"
+    [kernel_version_date]="uname -v | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}'"
+    [architecture]="arch"
+    [user_name]="whoami"
+    [os_ip]="hostname -I | tr ' ' '\n'"
+    [user_id]="id | awk -F'uid=' '{print \$2}' | grep -oE -m1 '^[0-9]{1,4}'"
+    [group_id]="id | awk -F'gid=' '{print \$2}' | grep -oE -m1 '^[0-9]{1,4}'"
+    [is_passwd_accessible]="./passwd_access.sh"
+)
+
+# execute and check
+declare -A results
+for key in "${!commands[@]}"; do
+    cmd="${commands[$key]}"
+    output=$(eval "$cmd" 2>&1)
+    if [[ $? -ne 0 ]]; then
+        echo "Error on ${key} : ${cmd}"
+        echo "Detail ${output}"
+    fi
+    results[$key]="$output"
+done
+
 # notify
-WEBHOOK="<webhook url>"
-MESSAGE="==========INTEL-REPORT==========/n
-        -----------user-----------n
-        User : ${user_name}/n
-        UID : ${user_id}/n
-        GID : ${group_id}/n
-        -----------uname-----------n
-        Os : ${OST_name}/n
-        Processor : ${processor}/n
-        Kerner Version Date : ${kernel_version_date}/n
-        -----------others-----------n
-        Addresses : ${os_ip}/n
-        Access to password file ? : ${is_passwd_accessible}/n
+WEBHOOK="<webhook-url>"
+MESSAGE="==========INTEL-REPORT==========
+        -----------user-----------
+        User : ${results[user_name]}
+        UID : ${results[user_id]}
+        GID : ${results[group_id]}
+        -----------uname-----------
+        Os : ${results[OS_name]}
+        Processor : ${results[processor]}
+        Kernel Version Date : ${results[kernel_version_date]}
+        -----------others-----------
+        Addresses : ${results[os_ip]}
+        Access to password file ? : ${results[is_passwd_accessible]}
         "
 
-curl -H 'Content-type: application/json' -X POST -d '{"content" : "${MESSAGE}"}' "${WEBHOOK}"
+# -n null input : create json object from nothing
+JSON_PAYLOAD=$(jq -n --arg msg "$MESSAGE" '{ 
+  "embeds": [{
+    "title": "OS Intelligence report",
+    "description": $msg,
+    "color": 9109504
+  }]
+}')
+curl -H "Content-Type: application/json" -X POST -d "$JSON_PAYLOAD" "$WEBHOOK"
